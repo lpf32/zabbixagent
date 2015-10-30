7d8b48eccfb39e389b27de0894c45844b51ae132
zhangpanpan <zhpan188@gmail.com>
2015-10-30 16:06:49 +0800

    set the send interval to the least interval of items

--- zabbixagent/controller.py
+++ zabbixagent/controller.py
@@ -10,24 +10,21 @@ class ItemProcessingController(object):
     def __init__(self, config, shutdown_event, items, database, sender, queue):
         self._shutdown_event = shutdown_event
         self._items = items
-        self._next_send_date = time()
         self._config = config
         self._database = database
         self._sender = sender
         self._logger = get_logger()
-        self._update_interval = None
         self._queue = queue
-        self._send_date = None
 
-    def _setup_update_interval(self):
+    def _setup_update_send_interval(self):
         minimun_update_interval = self._config.get('main', 'update_interval')
         for item in self._items:
             interval = item.get_update_interval()
             minimun_update_interval = min(interval, minimun_update_interval)
-        self._update_interval = float(minimun_update_interval) / 2
+        self._sender._send_interval = float(minimun_update_interval)
 
     def start(self):
-        self._setup_update_interval()
+        self._setup_update_send_interval()
         self._start_items()
         self._process_until_shutdown()
 
@@ -46,12 +43,6 @@ class ItemProcessingController(object):
     def _start_items(self):
         for item in self._items:
             item.start()
-
-    def _update_next_sender_date(self):
-        self._next_send_date = time() + self._update_interval
-
-    def _store_send_date(self):
-        self._send_date = time()
 
     def _write_item_values_to_datase(self, item_values):
         self._database.insert_values(item_values)
